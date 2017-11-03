9a139eba011af6e76030981565def0b039037de1
sanelesazi <sanelesazi@gmail.com>
2017-11-03 15:17:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 curl https://raw.githubusercontent.com/sanelesazi/vimsettings/master/vimrc_settings.txt >> ~/.vimrc
+echo "Vim settings Done!"
+open -a /Applications/Slack.app
+echo "Check For Slack..."
+open -a /Applications/Backup\ and\ Sync.app
+echo "Check Google Backup and Sync..."
+open -a /Applications/Google\ Chrome.app
+echo "Check Chrome..."
+open -a /Applications/iTerm.app
+echo "Check iTerm2"
 echo "Done! :-)"
